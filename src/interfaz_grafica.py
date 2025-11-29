2e5a48b5053b39807118b9706bddf5e94c11b6d6
AlinCervantes <zenikninq1822@gmail.com>
2025-11-28 19:00:02 -0600

    Solución de error al importar en interfaz grafica

--- src/interfaz_grafica.py
+++ src/interfaz_grafica.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox, scrolledtext
+from tkinter import ttk, messagebox, scrolledtext, simpledialog
 from calculadora import Calculadora
 import math
 
